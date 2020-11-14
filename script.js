157950f0a90eea30da0b1f3ad40507abd92f79dd
James Masaviru <jmasaviru@gmail.com>
2020-11-14 09:35:10 -0500

    Updating js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var currentTime = moment().format("HH");
 var currentTimeInt = parseInt(currentTime);
 var saveBtn = $(".saveBtn");
 
-//Time Blocks Data Attributes
+//data attributes for time blocks
 $("#9Block").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
 $("#10Block").attr("data-time", moment("10:00 am", "h:mm a").format("HH"));
 $("#11Block").attr("data-time", moment("11:00 am", "h:mm a").format("HH"));
@@ -14,22 +14,22 @@ $("#4Block").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
 $("#5Block").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
 
 $(document).ready(function(){
-
+   
     //get input events from stored local data
     //calls function
     renderEvents();
 
-    //set current day
-    var currentDay = moment().format("dddd,MMMM Do");
-    $("#currentDay").text(currentDay);
+     //set current day
+     var currentDay = moment().format("dddd,MMMM Do");
+     $("#currentDay").text(currentDay);
 
-    //change time block colors
+     //change time block colors
     for (var i=1; i <=12; i++){
         var inputTime = $("#" + i + "Block").attr("data-time");
         var inputTimeInt = parseInt(inputTime);
 
-        //if time block is current time
-        if(currentTimeInt === inputTimeInt){
+         //if time block is current time
+         if(currentTimeInt === inputTimeInt){
             $("#" + i + "Block").addClass("present");
         }
 
@@ -38,12 +38,12 @@ $(document).ready(function(){
             $("#" + i + "Block").addClass("past");
         }
 
-          //if time block is in the future
-          if(currentTimeInt < inputTimeInt){
+        //if time block is in the future
+        if(currentTimeInt < inputTimeInt){
             $("#" + i + "Block").addClass("future");
         }
     }
-    
+
     //button hover function
     saveBtn.on("mouseenter", function(){
         $(this).addClass("hover");
@@ -56,11 +56,11 @@ $(document).ready(function(){
     saveBtn.on("click", function(){
         var hour = $(this).attr("data-hour");
         var event = $("#" + hour + "Block").val();
-        //save event to local storage
+        //save event to localstorage
         localStorage.setItem(hour, event);
     });
 
-    //retrieve stored local event input 
+    //retrieve stored loacl event input 
     function renderEvents(){
         for (var i=1; i <=12; i++){
             $("#" + i +"Block").val(localStorage.getItem(i));
