0355e7f969a01d694bd25200caeeda8f5b634f61
James Masaviru <jmasaviru@gmail.com>
2020-11-13 20:34:00 -0500

    updating Time blocks

--- sctipt.js
+++ sctipt.js
@@ -40,11 +40,36 @@ $(document).ready(function(){
 
         //if time block is in the past
         if(currentTimeInt > inputTimeInt){
-            $("#" + i + "Row").addClass("past");
+            $("#" + i + "Block").addClass("past");
         }
 
           //if time block is in the future
           if(currentTimeInt < inputTimeInt){
-            $("#" + i + "Row").addClass("future");
+            $("#" + i + "Block").addClass("future");
         }
     }
+    
+    //button hover function
+    saveBtn.on("mouseenter", function(){
+        $(this).addClass("hover");
+    });
+    saveBtn.on("mouseleave", function(){
+        $(this).removeClass("hover");
+    });
+
+    // button click to save event
+    saveBtn.on("click", function(){
+        var hour = $(this).attr("data-hour");
+        var event = $("#" + hour + "Block").val();
+        //save event to local storage
+        localStorage.setItem(hour, event);
+    });
+
+    //retrieve stored local event input 
+    function renderEvents(){
+        for (var i=1; i <=12; i++){
+            $("#" + i +"Block").val(localStorage.getItem(i));
+        }
+    }
+
+});
